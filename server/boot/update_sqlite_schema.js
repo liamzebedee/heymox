c9efd26eebcd1b16198b6f450221d9cc1b97310a
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-02-23 15:51:58 +1000

    to many works

--- server/boot/update_sqlite_schema.js
+++ server/boot/update_sqlite_schema.js
@@ -1,7 +1,8 @@
   module.exports = function(app) {
+    app.dataSources.actualdb.autoupdate();
     app.dataSources.actualdb.isActual(function(err, actual){
       if (!actual) {
-        app.dataSources.actualdb.autoupdate();
+        
       }
     });
   };
